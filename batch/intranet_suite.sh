d65851fc0c03e779efc1595131f5f663c190e60b
oa-ciws <cooperationiwseu@gmail.com>
2008-11-05 08:26:06 +0000

    fix flavour release
    git-svn-id: svn+ssh://192.168.22.105/Project_1/branchs/ciws_fully_auto@152 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- batch/intranet_suite.sh
+++ batch/intranet_suite.sh
@@ -3,7 +3,7 @@
 cd /media/cooperation-core/cooperation/CVS/branch/Pro/ciws_fully_auto
 
 
-sudo ./cooperation-iws.py -c --createdirectories --isofile /media/partage-sauv/ISO/xubuntu-8.04.1-desktop-i386.iso --modulesfile templates/administrationModulesList_intranet --webappmirror http://localhost:81/depot --debmirror http://localhost:81/deb/mirror/ftp.proxad.net/mirrors/ftp.ubuntu.com/ubuntu/ --debmirrornonfree http://localhost:81/deb/mirror/packages.medibuntu.org/ --cms Wordpress --directory /media/partage-dev/1a4 --host administration --password cooperation --username ciwsadmin --artwork ciwsGnome --silent --outputisoname ciws-administration-intranet-0.5.2.iso
+sudo ./cooperation-iws.py -c --createdirectories --isofile /media/partage-sauv/ISO/xubuntu-8.04.1-desktop-i386.iso --modulesfile templates/administrationModulesList_intranet --webappmirror http://localhost:81/depot --debmirror http://localhost:81/deb/mirror/ftp.proxad.net/mirrors/ftp.ubuntu.com/ubuntu/ --debmirrornonfree http://localhost:81/deb/mirror/packages.medibuntu.org/ --cms Joomla --directory /media/partage-dev/1a4 --host administration --password cooperation --username ciwsadmin --artwork ciwsGnome --silent --outputisoname ciws-administration-intranet-0.5.2.iso
  
 cd /media/cooperation-core/cooperation/CVS/branch/Pro/ciws_fully_auto
 
