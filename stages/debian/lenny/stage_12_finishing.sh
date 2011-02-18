8ba353634461ad2ae3823e4be37ddfc15d666ca4
oa-ciws <cooperationiwseu@gmail.com>
2011-02-18 15:22:14 +0000

    fix stage 12
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@793 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- stages/debian/lenny/stage_12_finishing.sh
+++ stages/debian/lenny/stage_12_finishing.sh
@@ -12,7 +12,7 @@ LIVE_CD_DESC=""
 
 echo "I:FINISHING - CREATING NEW ISO FILE"
 
-cd $WORK_DIR ; find . -type f -print0 | xargs -0 md5sum > md5sum.txt
+cd $WORK_DIR/remaster ; find . -type f -print0 | xargs -0 md5sum > $WORK_DIR/remaster/md5sum.txt
 
 
 rm -Rf $WORK_DIR/$ISO_NAME
@@ -29,3 +29,6 @@ if [ "$ARCH" == "amd64" ]; then
 mkisofs -r -o $WORK_DIR/$ISO_NAME -b "isolinux/isolinux.bin" -c "isolinux/boot.cat" -no-emul-boot -V "$LIVE_CD_DESC" -J -l $WORK_DIR/remaster
 
 fi
+
+
+md5sum $WORK_DIR/$ISO_NAME > $WORK_DIR/$WORK_DIR/$ISO_NAME.md5
