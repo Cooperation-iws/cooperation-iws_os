5adb3c74d5aa84e7669c556613dfdb28b929ae49
oa-ciws <cooperationiwseu@gmail.com>
2011-02-22 13:31:09 +0000

    fix bash and templates
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@838 3e1a510d-c921-4b16-a628-562c3eed9ab9

---
+++ bash/bash-lenny.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+ciws_path="/home/esmalif/live-scripts/livescripts-trunk/"
+working_path="/home/esmalif/live-scripts/tmp/"
+iso_path="/home/esmalif/live-scripts/ISO-trunk/"
+template="/home/esmalif/work/template-openerp.sh"
+webappmirror="http://localhost:81/depot"
+debian_deb_mirror="http://localhost:3141/wwi.lokaleskif-b.intra/debian_mirror/debian/"
+debian_deb_mirror_nonfree="http://localhost:3141/wwi.lokaleskif-b.intra/debian_mirror/debian_multimedia/"
+debian_deb_mirror_security="http://localhost:3141/wwi.lokaleskif-b.intra/debian_mirror/updates/"
+log_path="/home/esmalif/data/log/"
+mirror_domain="localhost"
+
+#ubuntu_deb_mirror="http://$mirror_domain:3142/fr.archive.ubuntu.com/ubuntu/"
+ubuntu_deb_mirror="http://$mirror_domain:3142/wwi.lokaleskif-b.intra/jaunty-mirror/ubuntu/"
+
+ubuntu_deb_mirror_nonfree="http://$mirror_domain:3142/packages.medibuntu.org/"
+
+ubuntu_deb_mirror_security="http://$mirror_domain:3142/wwi.lokaleskif-b.intra/jaunty-mirror/ubuntu-security/"
+
+sudo ./cooperation-iws.py -c --createdirectories --silent --artwork ciws_gnome --dist debian --directory $working_path/1c42e1d9b8024c0552ffd8c8ca2ab760-2011-02-22-10-24-21 --outputisoname cooperation-iws-1c42e1d9b8024c0552ffd8c8ca2ab760-2011-02-22-10-24-20.iso --username ciwsadmin --password cooperation --host ciws-server --disautologin --encryption aes256 --encryptionpassphrase 04qwerty07197604121942* --isofile $iso_path/debian-live-gnome-full-crypt-110215.iso --distvers lenny --modulesfile $template --webappmirror $webappmirror --debmirror $debian_deb_mirror --debmirrornonfree $debian_deb_mirror_nonfree --debmirrorsecurity $debian_deb_mirror_security >> $log_path/ls-2011-02-22-10-24-20-0-1c42e1d9b8024c0552ffd8c8ca2ab760.log
