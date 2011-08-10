ed8efd5c474e454e15eae7c52948e58c2ca06b08
oa-ciws <cooperationiwseu@gmail.com>
2011-08-10 10:27:35 +0000

    adding Wordpress-3.2fr-book module
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@897 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- bash/templates/template-test-wheezy-amd64.sh
+++ bash/templates/template-test-wheezy-amd64.sh
@@ -6,7 +6,7 @@ gen_login_pwd="cooperation"
 apache_admin_pwd="$gen_login_pwd"
 mysql_root_password="mysql_root"
 mysql_generic_pwd="mysql_pwd"
-domain="my-erp.intra"
+domain="my-erp2.intra"
 #kernel wheezy upgrade
 kernel="2.6.39-2-amd64"
 #update live-initramfs
@@ -15,10 +15,13 @@ kernel="2.6.39-2-amd64"
 #NX server 0.2
 #Openssh server 0.2
 deactivate_ssh_root_login="y"
-#Postgresql 9.0
+#P#ostgresql 9.0
 root_psgsql_pwd="$mysql_root_password"
-#OpenERP 6.0.2
+#O#penERP 6.0.2
 openerp_name="Openerp_1"
 openerp_psgsql_pwd="$mysql_generic_pwd"
-
+#Wordpress 3.2fr-book
+wordpress_name[1]="Wordpress_1"
+wordpress_admin_email[1]="admin.local@$domain"
+wordpress_mysql_pwd[1]="$mysql_generic_pwd"
 #Mysql 5.1
