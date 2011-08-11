9a353dd389e545e5e2be167869b937f8b5298518
oa-ciws <cooperationiwseu@gmail.com>
2011-08-11 10:32:03 +0000

    adding trac, phpmyadmin, phppgadmin, mailserver modules
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@903 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- bash/templates/template-test-wheezy-amd64.sh
+++ bash/templates/template-test-wheezy-amd64.sh
@@ -11,12 +11,13 @@ domain="my-erp2.intra"
 kernel="2.6.39-2-amd64"
 #update live-initramfs
 #DNS server Wheezy
-#C#iwscms 0.9.2
+#Ciwscms 0.9.2
 #NX server 0.2
 #Openssh server 0.2
 deactivate_ssh_root_login="y"
 #P#ostgresql 9.0
 root_psgsql_pwd="$mysql_root_password"
+#P#hppgadmin 5.0.2
 #O#penERP 6.0.2
 openerp_name="Openerp_1"
 openerp_psgsql_pwd="$mysql_generic_pwd"
@@ -24,19 +25,25 @@ openerp_psgsql_pwd="$mysql_generic_pwd"
 wordpress_name[1]="Wordpress_1"
 wordpress_admin_email[1]="admin.local@$domain"
 wordpress_mysql_pwd[1]="$mysql_generic_pwd"
-#M#ysql 5.1
-#Clamtk 4.10
+#Mysql 5.1
+#Phpmyadmin 4.3.4.3
+#C#lamtk 4.10
 #Codecs Wheezy
-#Filezilla 0.1
-#Flashplayer Wheezy
-#Gimp 2.6
-#Iceweasel 0.1
-#Libre Office Wheezy
-#Mixxx 1.9
-#Secure delete 0.1
-#Sound juicer 0.1
-#Truecrypt 7.0a
-#Vlc 0.1
-#Kvm Wheezy
-#Xsane 0.1
+#F#ilezilla 0.1
+#F#lashplayer Wheezy
+#G#imp 2.6
+#I#ceweasel 0.1
+#L#ibre Office Wheezy
+#M#ixxx 1.9
+#S#ecure delete 0.1
+#S#ound juicer 0.1
+#T#ruecrypt 7.0a
+#V#lc 0.1
+#K#vm Wheezy
+#X#sane 0.1
+#T#rac Subversion 0.11.7
+svn_name[1]="Project_1"
+svn_admin_pwd[1]="$gen_login_pwd"
+svn_anonymous_pwd[1]="anonymous"
+trac_admin_pwd[1]="$gen_login_pwd"
 
