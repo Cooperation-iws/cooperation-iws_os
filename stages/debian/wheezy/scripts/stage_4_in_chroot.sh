052ade6834e82909cc5f2b1d3dbdd49f0923202a
oa-ciws <cooperationiwseu@gmail.com>
2011-08-21 11:16:57 +0000

    remove Lilina 1.0
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@962 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- stages/debian/wheezy/scripts/stage_4_in_chroot.sh
+++ stages/debian/wheezy/scripts/stage_4_in_chroot.sh
@@ -5,4 +5,34 @@
 #
 #
 
+. /tmp/scripts_params
 
+. /tmp/app_params
+
+
+#Building gnome-volume-manager
+
+if [ "$DEB_DIST" == "squeeze" ] ; then
+
+	#From source
+	#apt-get install --assume-yes --force-yes build-essential checkinstall
+	#apt-get build-dep gnome-volume-manager
+	#cd $DL_DIR
+	#mkdir g-v-m
+	#cd g-v-m
+	#apt-get source gnome-volume-manager
+	#cd gnome-volume-manager*
+	#./configure
+	#checkinstall -D
+#	apt-get install --assume-yes --force-yes hal
+#	if [ "$ARCH" == "amd64" ] ; then
+#		cd $DL_DIR
+#		wget $URL_CIWS_DEPOT/gnome-volume-manager_2.24.1-1_amd64.deb
+#		(echo "Y") | dpkg -i gnome-volume-manager_2.24.1-1_amd64.deb
+#	fi
+
+
+
+apt-get install --assume-yes --force-yes hal halevt gnome-device-manager
+
+fi
