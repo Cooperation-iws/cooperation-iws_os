1298b4f6637d37e54496bb6b7551c0b0e2973cc0
oa-ciws <cooperationiwseu@gmail.com>
2011-09-01 13:46:45 +0000

    fix some details
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@1003 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- bash/templates/template-test-wheezy-amd64.sh
+++ bash/templates/template-test-wheezy-amd64.sh
@@ -23,7 +23,7 @@ root_psgsql_pwd="$mysql_root_password"
 #O#penERP 6.0.2
 openerp_name="Openerp_1"
 openerp_psgsql_pwd="$mysql_generic_pwd"
-#W#ordpress 3.2fr-book
+#Wordpress 3.2fr-book
 wordpress_name[1]="Wordpress_1"
 wordpress_admin_email[1]="admin.local@$domain"
 wordpress_mysql_pwd[1]="$mysql_generic_pwd"
@@ -49,7 +49,7 @@ svn_name[1]="Project_1"
 svn_admin_pwd[1]="$gen_login_pwd"
 svn_anonymous_pwd[1]="anonymous"
 trac_admin_pwd[1]="$gen_login_pwd"
-#P#ostfix, Dovecot, PostfixAdmin, Clamav, Mailscanner Wheezy
+#Postfix, Dovecot, PostfixAdmin, Clamav, Mailscanner Wheezy
 postfixadmin_name="Mail_1"
 mail_domain="$domain"
 postfix_admin_email="admin.local@$domain"
@@ -60,7 +60,7 @@ postfix_relay_authentication_name=""
 postfix_relay_authentication_password=""
 mailbox_quota="0"
 #R#oundcube 0.5.6
-#F#engoffice 1.7.5
+#Fengoffice 1.7.5
 fengoffice_name[1]="Fengoffice_1"
 fengoffice_admin_email[1]="admin.local@$domain"
 fengoffice_mysql_pwd[1]="$mysql_generic_pwd"
@@ -71,7 +71,7 @@ egroupware_mysql_pwd[1]="$mysql_generic_pwd"
 #I#lohamail 0.8.14
 ilohamail_dbuser="Ilohamail"
 ilohamail_dbpwd="$mysql_generic_pwd"
-#H#orde 3.3source
+#Horde 3.3source
 horde_name="Horde_1"
 horde_mysql_pwd="$mysql_generic_pwd"
 #F#usionforge 5.0.3
@@ -83,12 +83,12 @@ redmine_name="Redmine"
 redmine_mysql_pwd="$mysql_generic_pwd"
 redmine_language="fr"
 #O#penxchange 6.20
-#A#mpache 3.5.4
+#Ampache 3.5.4
 ampache_name[1]="Ampache_1"
 ampache_mysql_pwd[1]="$mysql_generic_pwd"
-#L#ilina 2.0razor
+#Lilina 2.0razor
 lilina2_name[1]="Lilina2.0razor"
-#L#ilina 2.0classic
+#Lilina 2.0classic
 lilina2_classic_name[1]="Lilina2.0classic"
 #F#rontaccounting 2.2
 #Bugzilla 4.0.2
@@ -96,28 +96,28 @@ bugzilla_name[1]="Bugzilla_1"
 bugzilla_admin_email[1]="admin.local@$domain"
 bugzilla_mysql_pwd[1]="$mysql_generic_pwd"
 
-#M#nogosearch 3.3.11
-#A#pt-cacher-ng 0.6
-#N#top 3.4
+#Mnogosearch 3.3.11
+#Apt-cacher-ng 0.6
+#Ntop 3.4
 ntop_pwd=$gen_login_pwd
 #M#an2html 1.6
-#L#etoDMS 3.0.1
+#LetoDMS 3.0.1
 letodms_name[1]="Letodms_1"
 letodms_admin_email[1]="admin.local@$domain"
 letodms_mysql_pwd[1]="$mysql_generic_pwd"
 letodms_lang[1]="Francais"
 #Dpkg-www 2.54
-#O#js 2.3.6
+#Ojs 2.3.6
 ojs_name[1]="ojs_1"
 ojs_admin_email[1]="admin.local@$domain"
 ojs_mysql_pwd[1]="$mysql_generic_pwd"
 ojs_lang[1]="fr_CA"
-#O#cs 2.3.3
+#Ocs 2.3.3
 ocs_name[1]="ocs_1"
 ocs_admin_email[1]="admin.local@$domain"
 ocs_mysql_pwd[1]="$mysql_generic_pwd"
 ocs_lang[1]="fr_CA"
-#O#hs 2.3.1
+#Ohs 2.3.1
 ohs_name[1]="ohs_1"
 ohs_admin_email[1]="admin.local@$domain"
 ohs_mysql_pwd[1]="$mysql_generic_pwd"
@@ -130,47 +130,47 @@ lodel_lang[1]="fr_FR.UTF8"
 #Do#cbookwiki 0.9.2
 docbookwiki_pwd=$gen_login_pwd
 #Phpsysinfo 3.0
-#D#otproject 2.1.5
+#Dotproject 2.1.5
 dotproject_name[1]="Dotproject_1"
 dotproject_admin_email[1]="admin.local@$domain"
 dotproject_mysql_pwd[1]="$mysql_generic_pwd"
-#P#iwigo 2.2.4
+#Piwigo 2.2.4
 piwigo_name[1]="Piwigo_1"
 piwigo_admin_email[1]="admin.local@$domain"
 piwigo_mysql_pwd[1]="$mysql_generic_pwd"
 #S#quid 2.7
 #n#tp 4.2.6
 ntpd_server_address="192.168.30.254"
-#S#tatus.net 0.9.9
+#Status.net 0.9.9
 statusnet_name[1]="Statusnet_1"
 statusnet_admin_email[1]="admin.local@$domain"
 statusnet_mysql_pwd[1]="$mysql_generic_pwd"
-#E#lgg 1.7.11
+#Elgg 1.7.11
 elgg_name[1]="Elgg_1"
 elgg_admin_email[1]="admin.local@$domain"
 elgg_mysql_pwd[1]="$mysql_generic_pwd"
-#J#oomla 1.7.0demo
+#Joomla 1.7.0demo
 joomla_demo_name[1]="Joomla_demo_1"
 joomla_demo_admin_email[1]="admin.local@$domain"
 joomla_demo_mysql_pwd[1]="$mysql_generic_pwd"
-#J#oomla 1.7.0ciws
+#Joomla 1.7.0ciws
 joomla_ciws_name[1]="Joomla_ciws_1"
 joomla_ciws_admin_email[1]="admin.local@$domain"
 joomla_ciws_mysql_pwd[1]="$mysql_generic_pwd"
-#D#rupal 7.7
+#Drupal 7.7
 drupal_name[1]="Drupal_1"
 drupal_admin_email[1]="admin.local@$domain"
 drupal_mysql_pwd[1]="$mysql_generic_pwd"
-#M#ediawiki 1.17.0
+#Mediawiki 1.17.0
 mediawiki_name[1]="Mediawiki_1"
 mediawiki_admin_email[1]="admin.local@$domain"
 mediawiki_mysql_pwd[1]="$mysql_generic_pwd"
 mediawiki_lang[1]="fr"
-#D#rupal-commerce 7.7
+#Drupal-commerce 7.7
 drupal_commerce_name[1]="Drupal_commerce_1"
 drupal_commerce_admin_email[1]="admin.local@$domain"
 drupal_commerce_mysql_pwd[1]="$mysql_generic_pwd"
-#D#rupal-innovationnews 6.15
+#Drupal-innovationnews 6.15
 drupal_innovationnews_name[1]="Drupal_innovationnews_1"
 drupal_innovationnews_admin_email[1]="admin.local@$domain"
 drupal_innovationnews_mysql_pwd[1]="$mysql_generic_pwd"
@@ -194,15 +194,15 @@ openmeetings_name="Openmeetings_1"
 openmeetings_mysql_pwd="$mysql_generic_pwd"
 openmeetings_admin_email="admin.local@$domain"
 #R#emastersys 2.0
-#P#restashop 1.4
+#Prestashop 1.4
 prestashop_name[1]="Prestashop_1"
 prestashop_mysql_pwd[1]="$mysql_generic_pwd"
 prestashop_admin_email[1]="admin.local@$domain"
-#O#scommerce 3.0
+#Oscommerce 3.0
 oscommerce_name[1]="Oscommerce3_0_1"
 oscommerce_mysql_pwd[1]="$mysql_generic_pwd"
 oscommerce_admin_email[1]="admin.local@$domain"
-#O#scommerce 2.3.1
+#Oscommerce 2.3.1
 oscommerce_2_name[1]="Oscommerce2_3_1"
 oscommerce_2_mysql_pwd[1]="$mysql_generic_pwd"
 oscommerce_2_admin_email[1]="admin.local@$domain"
