c5093c52d7cba469ad14b5c63daa5b885d13e67d
oa-ciws <cooperationiwseu@gmail.com>
2012-01-07 17:31:50 +0000

    fix framework
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@1059 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- bash/bash-lenny-amd64.sh
+++ bash/bash-lenny-amd64.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ciws_path="/home/esmalif/live-scripts/livescripts-trunk/"
-working_path="/media/disk-1/tmp/"
-iso_path="/home/ciwsadmin/ISO-trunk"
-template="/home/ciwsadmin/bash/templates/template-nuxeo-lenny-amd64.sh"
-webappmirror="http://wwi.databases.intra:81/depot"
-debian_deb_mirror="http://wwi.databases.intra:3141/wwi.lokaleskif-b.intra/debian_mirror/debian/"
-debian_deb_mirror_nonfree="http://wwi.databases.intra:3141/wwi.lokaleskif-b.intra/debian_mirror/debian_multimedia/"
-debian_deb_mirror_security="http://wwi.databases.intra:3141/wwi.lokaleskif-b.intra/debian_mirror/updates/"
+working_path="/home/esmalif/livescript/dev/"
+iso_path="/home/esmalif/livescript/ISO-trunk/"
+template="/home/esmalif/livescript/livescripts-trunk/bash/templates/template-test-lenny-amd64.sh"
+webappmirror="http://wwi.lokaleskif-b.intra/depot/ciws-depot-lenny/"
+debian_deb_mirror="http://wwi.lokaleskif-b.intra/depot/lenny_mirror/debian/"
+debian_deb_mirror_nonfree="http://wwi.lokaleskif-b.intra/depot/lenny_mirror/debian_multimedia/"
+debian_deb_mirror_security="http://wwi.lokaleskif-b.intra/depot/lenny_mirror/debian/"
 log_path="/home/esmalif/data/log/"
 mirror_domain="localhost"
 
@@ -17,11 +17,11 @@ ubuntu_deb_mirror_nonfree="http://$mirror_domain:3142/packages.medibuntu.org/"
 
 ubuntu_deb_mirror_security="http://$mirror_domain:3142/wwi.lokaleskif-b.intra/jaunty-mirror/ubuntu-security/"
 
-apt-get install squashfs-tools mkisofs rsync syslinux
+apt-get install squashfs-tools genisoimage rsync syslinux
 
 
-mount -oremount,dev,exec /media/disk-1
+#mount -oremount,dev,exec /media/disk-1
 
-cd /home/ciwsadmin/livescripts-trunk
+cd /home/esmalif/livescript/livescripts-trunk
 
-sudo ./cooperation-iws.py -c --createdirectories --silent --artwork ciws_gnome --dist debian --directory $working_path/test27 --outputisoname cooperation-iws-1c42e1d9b8024c0552ffd8c8ca2ab760-2011-02-22-10-24-20.iso --username ciwsadmin --password cooperation --host ciws-server --disautologin --encryption aes256 --encryptionpassphrase 04qwerty07197604121942* --isofile $iso_path/lenny_gnome_amd64_crypt.iso --distvers lenny --modulesfile $template --webappmirror $webappmirror --debmirror $debian_deb_mirror --debmirrornonfree $debian_deb_mirror --arch amd64 --debmirrorsecurity $debian_deb_mirror_security
+sudo ./cooperation-iws.py -c --createdirectories --silent --artwork ciws_gnome --dist debian --directory $working_path/23 --outputisoname cooperation-iws-1c42e1d9b8024c0552ffd8c8ca2ab760-2011-02-22-10-24-20.iso --username ciwsadmin --password cooperation --host ciws-server --disautologin --encryption aes256 --encryptionpassphrase 04qwerty07197604121942* --isofile $iso_path/lenny-amd64-120107.iso --distvers lenny --arch amd64-wheezy --modulesfile $template --webappmirror $webappmirror --debmirror $debian_deb_mirror --debmirrornonfree $debian_deb_mirror_nonfree --debmirrorsecurity $debian_deb_mirror_security 
--- bash/bash-lenny.sh
+++ bash/bash-lenny.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 ciws_path="/home/esmalif/live-scripts/livescripts-trunk/"
-working_path="/home/esmalif/live-scripts/tmp/"
-iso_path="/home/esmalif/live-scripts/ISO-trunk/"
-template="/home/esmalif/work/template-openerp.sh"
-webappmirror="http://localhost:81/depot"
-debian_deb_mirror="http://localhost:3141/wwi.lokaleskif-b.intra/debian_mirror/debian/"
-debian_deb_mirror_nonfree="http://localhost:3141/wwi.lokaleskif-b.intra/debian_mirror/debian_multimedia/"
-debian_deb_mirror_security="http://localhost:3141/wwi.lokaleskif-b.intra/debian_mirror/updates/"
+working_path="/home/esmalif/livescript/dev/"
+iso_path="/home/esmalif/livescript/ISO-trunk/"
+template="/home/esmalif/livescript/livescripts-trunk/bash/templates/template-test-lenny-amd64.sh"
+webappmirror="http://wwi.lokaleskif-b.intra/depot/ciws-depot-lenny/"
+debian_deb_mirror="http://wwi.lokaleskif-b.intra/depot/lenny_mirror/debian/"
+debian_deb_mirror_nonfree="http://wwi.lokaleskif-b.intra/depot/lenny_mirror/debian_multimedia/"
+debian_deb_mirror_security="http://wwi.lokaleskif-b.intra/depot/lenny_mirror/debian/"
 log_path="/home/esmalif/data/log/"
 mirror_domain="localhost"
 
@@ -17,4 +17,4 @@ ubuntu_deb_mirror_nonfree="http://$mirror_domain:3142/packages.medibuntu.org/"
 
 ubuntu_deb_mirror_security="http://$mirror_domain:3142/wwi.lokaleskif-b.intra/jaunty-mirror/ubuntu-security/"
 
-sudo ./cooperation-iws.py -c --createdirectories --silent --artwork ciws_gnome --dist debian --directory $working_path/1c42e1d9b8024c0552ffd8c8ca2ab760-2011-02-22-10-24-21 --outputisoname cooperation-iws-1c42e1d9b8024c0552ffd8c8ca2ab760-2011-02-22-10-24-20.iso --username ciwsadmin --password cooperation --host ciws-server --disautologin --encryption aes256 --encryptionpassphrase 04qwerty07197604121942* --isofile $iso_path/debian-live-gnome-full-crypt-110215.iso --distvers lenny --modulesfile $template --webappmirror $webappmirror --debmirror $debian_deb_mirror --debmirrornonfree $debian_deb_mirror_nonfree --debmirrorsecurity $debian_deb_mirror_security >> $log_path/ls-2011-02-22-10-24-20-0-1c42e1d9b8024c0552ffd8c8ca2ab760.log
+sudo ./cooperation-iws.py -c --createdirectories --silent --artwork ciws_gnome --dist debian --directory $working_path/21 --outputisoname cooperation-iws-1c42e1d9b8024c0552ffd8c8ca2ab760-2011-02-22-10-24-20.iso --username ciwsadmin --password cooperation --host ciws-server --disautologin --encryption aes256 --encryptionpassphrase 04qwerty07197604121942* --isofile $iso_path/lenny_gnome_amd64_crypt.iso --distvers lenny --arch amd64-wheezy --modulesfile $template --webappmirror $webappmirror --debmirror $debian_deb_mirror --debmirrornonfree $debian_deb_mirror_nonfree --debmirrorsecurity $debian_deb_mirror_security >> $log_path/ls-2011-02-22-10-24-20-0-1c42e1d9b8024c0552ffd8c8ca2ab760.log
