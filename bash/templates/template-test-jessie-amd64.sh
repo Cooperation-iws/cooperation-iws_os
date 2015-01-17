e1d3a81a357175adedf2e580136aa36da550faf8
oa-ciws <cooperationiwseu@gmail.com>
2015-01-17 17:04:05 +0000

    add jessie gateway module
    git-svn-id: svn+ssh://192.168.22.105/Project_1/livescripts-trunk@1182 3e1a510d-c921-4b16-a628-562c3eed9ab9

--- bash/templates/template-test-jessie-amd64.sh
+++ bash/templates/template-test-jessie-amd64.sh
@@ -6,7 +6,7 @@ gen_login_pwd="cooperation"
 apache_admin_pwd="$gen_login_pwd"
 mysql_root_password="mysql_root"
 mysql_generic_pwd="mysql_pwd"
-domain="testjessie2.intra"
+domain="testjessie3.intra"
 #domain="testsqueeze-2.intra"
 keep_local_apt_parameters="y"
 #kernel jessie upgrade
@@ -23,11 +23,27 @@ console_mode="n"
 #N#X Client Jessie
 #Openssh server 0.2
 #W#inswitch Jessie
-#M#unin Jessie
+#Munin Jessie
 munin_password=$gen_login_pwd
-#Trac Subversion jessie
+#T#rac Subversion jessie
 svn_name[1]="Project_1"
 svn_admin_pwd[1]="$gen_login_pwd"
 svn_anonymous_pwd[1]="anonymous"
 trac_admin_pwd[1]="$gen_login_pwd"
-
+#G#ateway DNS 1.5.x
+gateway_nb_machine=20
+gateway_net_iface="eth0"
+gateway_local_iface="wlan0"
+range=19
+host_web_server="y"
+gateway_recursion="n"
+gateway_local_bridge="n"
+gateway_local_iface_2="eth2"
+gateway_multiple_isp="n"
+gateway_net_iface_2="eth2"
+hostapd="y"
+AP_interface=$gateway_local_iface
+AP_ssid="test78"
+AP_wpa_passphrase="0123456789"
+#Ntopng
+#Ossec 2.8 standalone
